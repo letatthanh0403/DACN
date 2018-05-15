bbd1c7fc2acdcea352e9b6474a7e3381e38fa10b
letatthanh0403 <32450713+letatthanh0403@users.noreply.github.com>
2018-05-16 00:21:49 +0700

    Add files via upload

---
+++ QLBH/QLBH/Control/NhanVienCtr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLBH.Object;
+using QLBH.Model;
+using System.Data;
+
+namespace QLBH.Control
+{
+    class NhanVienCtr
+    {
+        NhanVienModel nvMod = new NhanVienModel();
+        public DataTable GetData()
+        {
+            return nvMod.GetData();
+        }
+        public bool AddData(Nhanvienobj nvObj)
+        {
+            return nvMod.AddData(nvObj);
+        }
+        public bool UpdData(Nhanvienobj nvObj)
+        {
+            return nvMod.UpdData(nvObj);
+        }
+
+        public bool DelData(string ma)
+        {
+            return nvMod.DelData(ma);
+        }
+    }
+}
